90e875a887f28792097f06dbeeda1ac113ea4c90
Vyankatesh Poojari <65769298+vyankatesh96@users.noreply.github.com>
2020-06-10 21:42:43 +0530

    Add files via upload

---
+++ Geometry.java
@@ -0,0 +1,28 @@
+3. What will be the output of following program?
+
+public class PostIncrement {
+
+public static void main(String[] args) {
+
+int num1 = 1;
+
+int num2 = 1;
+
+num1++;
+
+num2++;
+
+System.out.println("num1 = " + num1);
+
+System.out.println("num2 = " + num2);
+
+}
+
+}
+
+
+
+**output**
+
+num1 = 2
+num2 = 2
